26a67b765a1e5ea79210e1b9a4ed4783fbeba9c5
Tony Liu <buptliuhs@gmail.com>
2016-01-14 14:35:27 +1300

    Update the launching script.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR=`dirname $0`
 export DYLD_LIBRARY_PATH=$DIR/lib:$DYLD_LIBRARY_PATH
 
 $DIR/login $@ $PARSE_APPLICATION_ID $PARSE_CLIENT_KEY
